7206b96526f8c492e82a0917abdeca28e2793b8d
PuXiongfei <pu.xiongfei@gmail.com>
2021-01-09 18:08:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,17 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.26.26/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/FF/g' package/base-files/files/bin/config_generate
+
+# Modify wifi name
+sed -i 's/OpenWrt/FF/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# luci-theme-argon theme
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
